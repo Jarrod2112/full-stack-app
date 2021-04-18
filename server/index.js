766e1fc5a41099844bbf172a70739d92e3875ea9
Jarrod Cabarubio <jarrodcabarubio7@gmail.com>
2021-04-17 19:26:05 -0500

    Adds incremented id

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const app = express();
-
 
 // GET, POST, PUT, PATCH, DELETE
 
@@ -10,49 +9,44 @@ app.use(express.json());
 
 // our database
 const posts = [
-    {
-        id: 0,
-        post: "Setting up my social media!",
-        timestamp: new Date(2002, 3, 7),
-        username: 'Jack'
-    }
+  {
+    id: 0,
+    post: "Setting up my social media!",
+    timestamp: new Date(2002, 3, 7),
+    username: "Jack",
+  },
 ];
 
 // 1. Add an incrementing id to new posts
 // 2. Add an endpoint to delete a post by id `DELETE /api/posts/:id`
 
-app.post('/api/posts', function (req, res) {
-    let id = req.body.id;
-    const post = req.body.posts;
-    const username = req.body.username;
-    //counter++;
-    for(i = 0; i < posts.length; i++ -1){
-        id = i;
-    }
-    posts.push(id);
-    let newPost = {
-        id: id,
-        post: post,
-        username: username,
-        timestamp: new Date(),
-    }    
+let id = 1; 
+app.post("/api/posts", function (req, res) {
+  const post = req.body.post;
+  const username = req.body.username;
+  const newPost = {
+      id: id,
+      post: post,
+      username: username,
+      timestamp: new Date(),
+    };
+    id++;
     posts.push(newPost);
     return res.json(newPost);
 });
 
 // Post Functionality
-app.get('/api/posts', function (req, res) {
-    return res.json(posts);
-    // res.send(JSON.stringify([]))
+app.get("/api/posts", function (req, res) {
+  return res.json(posts);
+  // res.send(JSON.stringify([]))
 });
 
 // Handle an HTTP GET request to /
-const publicAssets = path.join(__dirname, '../public');
-console.log('Serving public assets from ', publicAssets);
+const publicAssets = path.join(__dirname, "../public");
+console.log("Serving public assets from ", publicAssets);
 app.use(express.static(publicAssets));
-
 
 // This makes the server start listening to HTTP requests on port 4000
 app.listen(4000, () => {
-    console.log('Listening on http://localhost:4000');
+  console.log("Listening on http://localhost:4000");
 });
