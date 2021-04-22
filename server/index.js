5d78f67234cdff2472dc1f500284f6d6a19e0324
Jarrod Cabarubio <jarrodcabarubio7@gmail.com>
2021-04-21 21:57:07 -0500

    Added get all

--- server/index.js
+++ server/index.js
@@ -1,21 +1,28 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const mongo = require('./mongo/MongoSingleton');
 const apiRouter = require('./routes/api');
 
 // GET, POST, PUT, PATCH, DELETE
+async function run() {
+  await mongo.initialize('mongodb://localhost:27017/');
+  // this parses POST bodies as JSON
+  app.use(express.json());
 
-// this parses POST bodies as JSON
-app.use(express.json());
+  // Set up mongo connection 
+  // Set up API routes
+  app.use('/api', apiRouter);
 
-app.use('/api', apiRouter);
+  // Handle an HTTP GET request to /
+  const publicAssets = path.join(__dirname, "../public");
+  console.log("Serving public assets from ", publicAssets);
+  app.use(express.static(publicAssets));
 
-// Handle an HTTP GET request to /
-const publicAssets = path.join(__dirname, "../public");
-console.log("Serving public assets from ", publicAssets);
-app.use(express.static(publicAssets));
+  // This makes the server start listening to HTTP requests on port 4000
+  app.listen(4000, () => {
+    console.log("Listening on http://localhost:4000");
+  });
+}
 
-// This makes the server start listening to HTTP requests on port 4000
-app.listen(4000, () => {
-  console.log("Listening on http://localhost:4000");
-});
+run();
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,58 +1,43 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const db = require("../mongo/MongoSingleton");
 
 const postsRouter = Router();
 
-// our database
-const posts = [
-    {
-      id: 0,
-      post: "Setting up my social media!",
-      timestamp: new Date(2002, 3, 7),
-      username: "Jack",
-    },
-  ];
-  
-  // 1. Add an incrementing id to new posts
-  // 2. Add an endpoint to delete a post by id `DELETE /api/posts/:id`
-  
-  let id = 1; 
-  postsRouter.post("/", function (req, res) {
-    const post = req.body.post;
-    const username = req.body.username;
-    const newPost = {
-        id: id,
-        post: post,
-        username: username,
-        timestamp: new Date(),
-      };
-      id++;
-      posts.push(newPost);
-      return res.json(newPost);
-  });
-  /*
+postsRouter.post("/", async function (req, res) {
+  const post = req.body.post;
+  const username = req.body.username;
+  const newPost = {
+    post: post,
+    username: username,
+    timestamp: new Date(),
+  };
+  const result = await db.getInstance().collection("posts").insertOne(newPost);
+  return res.json(newPost);
+});
+/*
   There are a few ways for information/data to be passed in with a request:
   1. On the body of a request
   2. In a URL parameter
   3. In a query string
   */
-  
-  /**
-   * This endpoint specifies that a parameter in the position of
-   * :id will get added to req.params.id
-   */
-  postsRouter.delete("/:id", function (req, res) {
-      const id = parseInt(req.params.id); // all parameters come through as strings, you have to parse it yourself accordingly
-  
-      const postIndex = posts.findIndex(post => post.id === id);
-      posts.splice(postIndex, 1);
-  
-      return res.json({ id });
-  });
-  
-  // Post Functionality
-  postsRouter.get("/", function (req, res) {
-    return res.json(posts);
-    // res.send(JSON.stringify([]))
-  });
 
-  module.exports = postsRouter;
+/**
+ * This endpoint specifies that a parameter in the position of
+ * :id will get added to req.params.id
+ */
+postsRouter.get('/:id', async function(req,res) {
+  // get a post by id
+})
+
+postsRouter.delete("/:id", function (req, res) {
+  // delete a post by id
+});
+
+// Post Functionality
+postsRouter.get("/", async function (req, res) {
+  const result = await db.getInstance().collection("posts").find({}).toArray(); 
+  return res.json(result);
+  // res.send(JSON.stringify([]))
+});
+
+module.exports = postsRouter;
