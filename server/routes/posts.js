bc5d98cd4029cbc472eedc624ff45ee741b2c00c
Jarrod Cabarubio <jarrodcabarubio7@gmail.com>
2021-04-26 21:33:13 -0500

    Added find document by username

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,6 @@
 const { ObjectID } = require("bson");
-const { Router } = require("express");
+const { Router, json } = require("express");
+const { BSONType } = require("mongodb");
 const db = require("../mongo/MongoSingleton");
 
 const postsRouter = Router();
@@ -26,22 +27,32 @@ postsRouter.post("/", async function (req, res) {
  * This endpoint specifies that a parameter in the position of
  * :id will get added to req.params.id
  */
-postsRouter.get('/:id', async function (req, res) {
+postsRouter.get("/:username", async function (req, res) {
+  const user =  req.params.username;
+  const collection = db.getInstance().collection("posts");
+  let query = { username: user };
+  const result = await collection.findOne(query);
+  console.log(result);
+  return res.json(result);
+});
+
+postsRouter.get("/:id", async function (req, res) {
   // get a post by id
   const id = ObjectID(req.params.id);
   const collection = db.getInstance().collection("posts");
-  let query = { "_id": id };
+  let query = { _id: id };
   const cursor = await collection.findOne(query);
   return res.json(cursor);
-})
+});
 
-postsRouter.delete("/:id", function (req, res) {
+postsRouter.delete("/:id", async function (req, res) {
   const id = ObjectID(req.params.id);
   const collection = db.getInstance().collection("posts");
-  let query = { "_id": id };
-  const result = collection.deleteOne(query);
-  return res.json(result)
+  let query = { _id: id };
+  const result = await collection.deleteOne(query);
+  return res.json(result);
 });
+
 
 // Post Functionality
 postsRouter.get("/", async function (req, res) {
