2969be77b5189fe9d594ed9cb2b40c0ea33a103c
jarrod2112 <jarrodcabarubio7@gmail.com>
2021-10-30 18:39:54 -0500

    Added profile update endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const { ObjectId } = require("mongodb");
 const db = require("../mongo/MongoSingleton");
+const moment = require('moment')
 
 const usersRouter = Router();
 
@@ -16,6 +17,31 @@ usersRouter.get("/", async (req, res) => {
     res.status(404).send("NOT_FOUND");
   }
   return res.json(user);
+});
+
+// update the profile on the user
+// HTTP PATCH /api/users/profile
+usersRouter.patch("/profile", async (req, res) => {
+
+  const userId = req.user._id;
+  const collection = db.getInstance().collection("users");
+
+  // update profile fields
+  const firstName = req.user.profile.firstName || req.body.firstName;
+  const lastName = req.user.profile.lastName || req.body.lastName;
+  const phoneNumber = req.user.profile.phoneNumber || req.body.phoneNumber;
+  const email = req.user.profile.email || req.body.email;
+  let birthday = req.user.profile.birthday || req.body.birthday;
+
+  if(birthday) {
+    birthday = moment(birthday).toDate();
+  }
+
+  const newProfile = { firstName, lastName, birthday, phoneNumber, email };
+
+  await collection.updateOne({ _id: userId }, { $set: { profile: newProfile } });
+  // return something
+  return res.status(200).send('Profile created');
 });
 
 module.exports = usersRouter;
