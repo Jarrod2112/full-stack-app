6947c5b4f411ce582dd31c124878b7b93d8786e5
jarrod2112 <jarrodcabarubio7@gmail.com>
2021-11-05 11:28:02 -0500

    bootstrap

--- src/components/posts/profile.js
+++ src/components/posts/profile.js
@@ -24,34 +24,46 @@ export const Profile = (props) => {
   }, []);
 
   return (
-    <div>
-      <div>
-        <h6>First Name</h6>
-        <input
-          type="text"
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <h6>Last Name</h6>
-        <input
-          type="text"
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <h6>Phone Number</h6>
-        <input
-          type="text"
-          onChange={(e) => setPhonenumber(e.target.value)}
-        />
-        <h6>E-mail</h6>
-        <input
-          type="text"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <h6>Birthday</h6>
-        <input
-          type="text"
-          onChange={(e) => setBirthday(e.target.value)}
-        />
-        <button onClick={handleClick}>Save</button>
+    <div class="card text-white bg-dark mb-3 d-flex">
+      <div className="d-flex">
+        <div>
+          <div>
+            <h6>First Name</h6>
+            <input
+              className="form-control me-2"
+              type="text"
+              placeholder={profile.firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <h6>Last Name</h6>
+            <input
+              className="form-control me-2"
+              type="text"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <h6>Phone Number</h6>
+            <input
+              className="form-control me-2"
+              type="text"
+              onChange={(e) => setPhonenumber(e.target.value)}
+            />
+            <h6>E-mail</h6>
+            <input
+              className="form-control me-2"
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <h6>Birthday</h6>
+            <input
+              className="form-control me-2"
+              type="text"
+              onChange={(e) => setBirthday(e.target.value)}
+            />
+            <button
+              className="btn btn-outline-success"
+              onClick={handleClick}>Save</button>
+          </div>
+        </div>
       </div>
     </div>
   )
